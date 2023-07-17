eff8179e6563265646b174facc1689e1cce205df
stefank1995 <stefan.kolaric4@gmail.com>
2023-07-17 17:03:29 +0200

    Add project files.

---
+++ SignalRChatRoomApp/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatRoomApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        public ChatHub()
+        {
+            _botUser = "MyChat bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Clients.All.SendAsync("ReceieveMessage", "Hey everyone");
+        }
+    }
+}
